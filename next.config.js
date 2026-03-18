b229d8785386a27cf3d6c981ec94cf450e169041
zerohzz <alexander.zzzz@hotmail.com>
2026-03-18 21:08:24 +1100

    fix: add CORS headers to allow iframe widget to load

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
   rewrites() {
     return [
       {
-        source: '/doc',
-        destination: '/doc/index.html'
+        source: "/doc",
+        destination: "/doc/index.html"
       }
     ]
   }
